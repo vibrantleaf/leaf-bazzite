6ecd1df9a8372a85360871612778a75b2e47cdec
julia <sea.leaf@outlook.com>
2024-09-16 17:50:07 +0800

    added system_files and fixed build

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 
-rpm-ostree install libvirt-client qemu-kvm virt-install virt-manager virt-viewer swtpm tuned bridge-utils
-
-rpm-ostree kargs --append-if-missing="amd_iommu=on" --append-if-missing="iommu=pt" --append-if-missing="kvm_amd.nested=1" --append-if-missing="vfio_iommu_type1.allow_unsafe_interrupts=1" --append-if-missing="kvm.ignore_msrs=1" --append-if-missing="default_huepagesz=1G" --append-if-missing="hugepagesz=1G"
+rpm-ostree install libvirt-client \
+  qemu-kvm \
+  virt-install \
+  virt-manager \
+  virt-viewer \
+  swtpm \
+  tuned \
+  bridge-utils
 
 systemctl enable podman.socket
