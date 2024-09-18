b6d1d56a65f9e269e5493b4b64d0c53c6ac87031
julia <sea.leaf@outlook.com>
2024-09-18 17:42:51 +0800

    updated build.sh

--- Containerfile
+++ Containerfile
@@ -5,11 +5,14 @@ ARG SOURCE_TAG="latest"
 FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
 
 COPY build.sh /tmp/build.sh
+
 COPY system_files/usr /usr
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
 
-RUN rm -rf /var/cache/* /var/tmp/* && \
+RUN rm -rf /var/cache/* && \
+    rm -rf /var/tmp/* && \
+    /usr/libexec/containerbuild/cleanup.sh && \
     ostree container commit
--- build.sh
+++ build.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+# start of boiler plate
+
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
 systemctl enable podman.socket
 
+# End of boiler plate
 
+# Start of COPR/rpm-repos
+# yoinked from bazzite + added matte-schwartz/sunshine
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-bazzite.repo https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-${RELEASE}/kylegospo-bazzite-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite-multilib/repo/fedora-${RELEASE}/kylegospo-bazzite-multilib-fedora-${RELEASE}.repo?arch=x86_64
+curl -Lo /etc/yum.repos.d/_copr_ublue-os-staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-${RELEASE}/ublue-os-staging-fedora-${RELEASE}.repo?arch=x86_64
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-latencyflex.repo https://copr.fedorainfracloud.org/coprs/kylegospo/LatencyFleX/repo/fedora-${RELEASE}/kylegospo-LatencyFleX-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-obs-vkcapture.repo https://copr.fedorainfracloud.org/coprs/kylegospo/obs-vkcapture/repo/fedora-${RELEASE}/kylegospo-obs-vkcapture-fedora-${RELEASE}.repo?arch=x86_64
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-wallpaper-engine-kde-plugin.repo https://copr.fedorainfracloud.org/coprs/kylegospo/wallpaper-engine-kde-plugin/repo/fedora-${RELEASE}/kylegospo-wallpaper-engine-kde-plugin-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_ycollet-audinux.repo https://copr.fedorainfracloud.org/coprs/ycollet/audinux/repo/fedora-${RELEASE}/ycollet-audinux-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-rom-properties.repo https://copr.fedorainfracloud.org/coprs/kylegospo/rom-properties/repo/fedora-${RELEASE}/kylegospo-rom-properties-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-webapp-manager.repo https://copr.fedorainfracloud.org/coprs/kylegospo/webapp-manager/repo/fedora-${RELEASE}/kylegospo-webapp-manager-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_hhd-dev-hhd.repo https://copr.fedorainfracloud.org/coprs/hhd-dev/hhd/repo/fedora-${RELEASE}/hhd-dev-hhd-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts.repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-${RELEASE}/che-nerd-fonts-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_sentry-switcheroo-control_discrete.repo https://copr.fedorainfracloud.org/coprs/sentry/switcheroo-control_discrete/repo/fedora-${RELEASE}/sentry-switcheroo-control_discrete-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_hikariknight-looking-glass-kvmfr.repo https://copr.fedorainfracloud.org/coprs/hikariknight/looking-glass-kvmfr/repo/fedora-${RELEASE}/hikariknight-looking-glass-kvmfr-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_rok-cdemu.repo https://copr.fedorainfracloud.org/coprs/rok/cdemu/repo/fedora-${RELEASE}/rok-cdemu-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_rodoma92-kde-cdemu-manager.repo https://copr.fedorainfracloud.org/coprs/rodoma92/kde-cdemu-manager/repo/fedora-${RELEASE}/rodoma92-kde-cdemu-manager-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/_copr_rodoma92-rmlint.repo https://copr.fedorainfracloud.org/coprs/rodoma92/rmlint/repo/fedora-${RELEASE}/rodoma92-rmlint-fedora-${RELEASE}.repo
+curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+curl -Lo /etc/yum.repos.d/_copr_matte-schwartz_sunshine.repo https://copr.fedorainfracloud.org/coprs/matte-schwartz/sunshine/repo/fedora-${RELEASE}/matte-schwartz-sunshine-fedora-${RELEASE}.repo
+sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo
+# End of COPR/rpm-repos
 
 # Start of Virtualization stack
 
@@ -36,12 +61,17 @@ echo "Finished Installing The Virtualization Stack"
 # Start of Sunshine
 
 echo "Started Installing Sunshine"
-echo "Grabbing The lizardbyte Sunshine COPR"
-curl -Lo /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo https://copr.fedorainfracloud.org/coprs/lizardbyte/Sunshine/repo/fedora-$RELEASE/lizardbyte-Sunshine-fedora-$RELEASE.repo
-sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo  #hack to just to get this to build NEED TO FIX LATER
-echo "Installing The sunshine Package"
 rpm-ostree install sunshine
 echo "Removing The Lizardbyte sunshine COPR"
-rm /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo
-
 # End of Sunshine
+
+# Start of Misc
+echo "Installing Miscellaneous Packeages"
+rpm-ostree install git \
+  stow \
+  android-tools \
+  ImageMagick \
+  ffmpeg
+echo "Finished Installing Miscellaneous Packeages"
+
+# End of Misc
