0357e0be6fa364e5290becd6b2496dbcdb84ee83
julia <sea.leaf@outlook.com>
2024-09-18 15:23:49 +0800

    updated build.sh

--- Containerfile
+++ Containerfile
@@ -4,16 +4,12 @@ ARG SOURCE_TAG="latest"
 
 FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
 
-
-
 COPY build.sh /tmp/build.sh
-COPY system_files/* /
-
+COPY system_files/usr /usr
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
-## NOTES:
-# - /var/lib/alternatives is required to prevent failure with some RPM installs
-# - All RUN commands must end with ostree container commit
-#   see: https://coreos.github.io/rpm-ostree/container/#using-ostree-container-commit
+
+RUN rm -rf /var/cache/* /var/tmp/* && \
+    ostree container commit
--- build.sh
+++ build.sh
@@ -4,7 +4,14 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+systemctl enable podman.socket
 
+
+
+# Start of Virtualization stack
+
+echo "Started Installing The Virtualization Stack"
+echo "Installing LibVirt & tools"
 rpm-ostree install libvirt-client \
   qemu-kvm \
   virt-install \
@@ -13,5 +20,29 @@ rpm-ostree install libvirt-client \
   swtpm \
   tuned \
   bridge-utils
+echo "Making sure the LibVirt Monolithic Service & Socket is Disabled"
+systemctl disable libvirt.service
+systemctl disable libvirt.socket
+echo "Enabling the LibVirt Dynamic Services & Sockets"
+for drv in qemu network nodedev nwfilter secret storage
+do
+  systemctl enable virt${drv}d.service
+  systemctl enable virt${drv}d{,-ro,-admin}.socket
+done
+echo "Finished Installing The Virtualization Stack"
 
-systemctl enable podman.socket
+# End of Virtualization stack
+
+# Start of Sunshine
+
+echo "Started Installing Sunshine"
+echo "Grabbing The lizardbyte Sunshine COPR"
+curl -Lo /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo https://copr.fedorainfracloud.org/coprs/lizardbyte/Sunshine/repo/fedora-$RELEASE/lizardbyte-Sunshine-fedora-$RELEASE.repo
+curl -Lo /var/cache/lizardbyte-Sunshine-fedora.gpg https://download.copr.fedorainfracloud.org/results/lizardbyte/Sunshine/pubkey.gpg
+sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo  #hack to just to get this to build NEED TO FIX LATER
+echo "Installing The sunshine Package"
+rpm-ostree install sunshine
+echo "Removing The Lizardbyte sunshine COPR"
+rm /etc/yum.repos.d/lizardbyte-Sunshine-fedora-$RELEASE.repo
+
+# End of Sunshine
